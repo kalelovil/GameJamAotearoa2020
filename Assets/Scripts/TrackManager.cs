2ec0ba69d7aacba220925dbe0bfbc2e6c6394436
DESKTOP-UINPFT3\kalel <kalelovil@gmail.com>
2020-08-02 01:50:09 +1200

    - tmp auto resources - path for targets to follow

---
+++ Assets/Scripts/TrackManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TrackManager : MonoBehaviour
+{
+    [SerializeField] bool _hasChanged;
+
+    [SerializeField] List<TrackWaypoint> _trackWaypointList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+        _trackWaypointList = transform.GetComponentsInChildren<TrackWaypoint>().ToList();
+        TrackWaypoint curWaypoint, nextWaypoint;
+        for (int i = 0; i < _trackWaypointList.Count; i++)
+        {
+            curWaypoint = _trackWaypointList[i];
+            nextWaypoint = _trackWaypointList[(i + 1) % _trackWaypointList.Count];
+
+            curWaypoint._numText.text = $"{i + 1}";
+            curWaypoint._lineRenderer.positionCount = 2;
+            curWaypoint._lineRenderer.SetPosition(0, curWaypoint.transform.position);
+            curWaypoint._lineRenderer.SetPosition(1, nextWaypoint.transform.position);
+        }
+
+        _hasChanged = false;
+    }
+
+    private void LateUpdate()
+    {
+    }
+}
---
+++ Assets/Scripts/TrackWaypoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TrackWaypoint : MonoBehaviour
+{
+    [SerializeField] internal TextMeshPro _numText;
+    [SerializeField] internal LineRenderer _lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
